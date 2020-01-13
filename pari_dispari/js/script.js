809314a005b0b811b9f7fc1be55109ff3caddd7a
simona <palumbosimona118@gmail.com>
2020-01-13 19:38:21 +0100

    ex pari-dispari

---
+++ pari_dispari/js/script.js
@@ -0,0 +1,11 @@
+//L’utente sceglie pari o dispari e un numero da 1 a 5.
+//Generiamo un numero random (sempre da 1
+//a 5) per il computer.
+//Sommiamo i due numeri
+//dichiariamo chi ha vinto (controlliamo se la somma da pari o dispari).
+
+var pariDispari = prompt('pari o dispari?')
+console.log(pariDispari);
+
+var numero = parseInt(prompt('inserisci un numero da 1 a 5'));
+console.log(numero);
