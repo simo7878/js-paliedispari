4013b857f4febdb1b810324c65f313583b2d8ca8
simona <palumbosimona118@gmail.com>
2020-01-13 20:13:12 +0100

    commit finale

--- pari_dispari/js/script.js
+++ pari_dispari/js/script.js
@@ -9,3 +9,15 @@ console.log(pariDispari);
 
 var numero = parseInt(prompt('inserisci un numero da 1 a 5'));
 console.log(numero);
+
+var numeroRandom = Math.floor(Math.Random() * 5) + 1;
+console.log(numeroRandom);
+
+var numeroEstratto = estrazione(numero, numeroRandom);
+if (numeroEstratto) = estrazione(numero, numeroRandom);
+
+if (numeroEstratto == pariDispari) {
+  alert('hai vinto');
+} else {
+  return 'dispari';
+}
